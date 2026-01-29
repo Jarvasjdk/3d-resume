0ad365822b1ad38e42b5fc6e96680887e32367ea
Nathanael Davis <nathanaeldavis@Nathanaels-Air.lan>
2026-01-29 15:05:52 -0600

    third

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,14 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     base: '/3d-resume/',
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    three: ['three'],
+                    gsap: ['gsap'],
+                }
+            }
+        }
+    }
 });
